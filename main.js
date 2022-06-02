20aa67755ec776bd0c341449a6fec3621f281ab2
Khuhani <suleimanmigwi@gmail.com>
2022-06-02 15:59:44 +0300

    Final Version

--- main.js
+++ main.js
@@ -1,17 +1,37 @@
 const container = document.querySelector('.container');
-const size = document.querySelector('.size').value;
+const sizeChange = document.querySelector('.size');
+let size = sizeChange.value;
 const color = document.querySelector('.color');
+const clear = document.querySelector('.btn');
+//const erase = document.querySelector('.erase');
 
 function populate(size){
     container.style.setProperty('--size',size);
     for(let i=0; i < size * size; i++){
         const div = document.createElement('div');
         div.classList.add('pixel');
-        div.addEventListener('mouseover', function (){
+        div.addEventListener('mouseover', () => { 
+            if(!draw)return 
             div.style.backgroundColor = color.value});
-        
+        div.addEventListener('mousedown',() =>{
+            div.style.backgroundColor = color.value
+        })
         container.appendChild(div);
     }
 }
 
+container.addEventListener('mousedown',() => draw=true)
+container.addEventListener('mouseup',() => draw=false)
+
 populate(size);
+
+function reset(){
+    container.innerHTML='';
+    populate(size);
+}
+
+clear.addEventListener('click',reset);
+
+sizeChange.addEventListener('keyup', () => {size = sizeChange.value;reset();});
+
+//erase.addEventListener('mousedown',() => {div.style.backgroundColor = 'white';});
